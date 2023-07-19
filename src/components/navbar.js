04786822b27c75473d1045905eef65abb113ce4c
unknown <alimoussa06@gmail.com>
2023-07-19 04:22:11 +0100

    Configure React app for deployment to GitHub Pages

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
         <nav>
             <div className="container-fluid">
 
-                <span className="navbar-brand mb-0 h1 ms-5">ADD</span>
+                <span className="navbar-brand mb-0 h1 ms-5">TEST-ADAA</span>
                 <div className="navbar-nav me-5">
                     <a className="nav-link active h5 " aria-current="page" href="/">ACCUEIL</a>
                 </div>
