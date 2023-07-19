ba08fc45f1c23d6fb8121c6abaf3d11a28a6ed55
unknown <alimoussa06@gmail.com>
2023-07-19 04:08:39 +0100

    Configure React app for deployment to GitHub Pages

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,30 +1,30 @@
 import React from 'react';
 
-const SearchBar = ({handleSubmit,query,setQuery,  selectItem, setSelectItem}) => (
-    <form className="row mb-4" onSubmit={handleSubmit}>
+const SearchBar = ({ handleSubmit, query, setQuery, selectItem, setSelectItem }) => (
+  <form className="row mb-4" onSubmit={handleSubmit}>
 
-              <div className="form-group col-md-4">
-                <input type="text"
-                  name="search_box" id="search_box"
-                  className="form-control" placeholder="Recherche..."
-                  value={query || ''}
-                  onChange={(e) => setQuery(e.target.value)} />
-              </div>
-              <div className="form-group col-md-2">
-                <button type="submit" className="btn btn-primary form-control">Rechercher</button>
-              </div>
-              <div className="form-group col-md-3 ms-5 d-flex gap-auto">
-                <span id="select" className="">Item par page</span>
-                <select  aria-labelledby="select" className="form-control" defaultValue={'4'} value={selectItem}
-                  onChange={e => setSelectItem(e.target.value)}>
-                  <option value={'4'}>4</option>
-                  <option value={'10'}>10</option>
-                  <option value={'20'}>20</option>
-                  <option value={'30'}> 30</option>
-                  <option value={'40'}>40</option>
-                </select>
-              </div>
-            </form>
+    <div className="form-group col-md-4 col-offset-sm-3">
+      <input type="text"
+        name="search_box" id="search_box"
+        className="form-control" placeholder="Recherche..."
+        value={query || ''}
+        onChange={(e) => setQuery(e.target.value)} />
+    </div>
+    <div className="form-group col-md-2 col-offset-md-4">
+      <button type="submit" className="btn btn-primary form-control">Rechercher</button>
+    </div>
+    <div className="form-group col-md-3 col-offset-md-3 d-flex gap-auto">
+      <span id="select" className="">Item par page</span>
+      <select aria-labelledby="select" className="form-control" defaultValue={'4'} value={selectItem}
+        onChange={e => setSelectItem(e.target.value)}>
+        <option value={'4'}>4</option>
+        <option value={'10'}>10</option>
+        <option value={'20'}>20</option>
+        <option value={'30'}> 30</option>
+        <option value={'40'}>40</option>
+      </select>
+    </div>
+  </form>
 
 
 )
