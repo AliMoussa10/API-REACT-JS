1623c3a693f69f1f57c8cde0ff12be8ed2eaf978
unknown <alimoussa06@gmail.com>
2023-07-19 03:01:04 +0100

    Configure React app for deployment to GitHub Pages

--- src/components/table.js
+++ src/components/table.js
@@ -8,7 +8,7 @@ const List = ({ Data }) => (
         Data.map((dataObj, index) => (
             <tr key={index}>
             <td>{dataObj.id}</td>
-            <td> <img src={dataObj.thumbnail} alt="icons" className="img-responsive rounded-circle" /></td>
+            <td> <img src={dataObj.thumbnail} alt="icons" className="img-responsive rounded" width={'100'} height={'90'}/></td>
             <td>{dataObj.title}</td>
             <td>{dataObj.description}</td>
             <td>{dataObj.price}</td>
